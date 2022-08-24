016664b0479f9a0d8fd98207939696f7a9170927
Haelmorn <haelmorn@gmail.com>
2022-08-24 10:45:45 +0200

    Removed old import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
-
 
 // https://astro.build/config
 export default defineConfig({
