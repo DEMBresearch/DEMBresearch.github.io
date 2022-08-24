258f4696a91f75da88542d8774bee0e150ecf669
Haelmorn <haelmorn@gmail.com>
2022-08-24 20:28:25 +0200

    Trigger rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,5 @@ export default defineConfig({
   // Enable React for the Algolia search component.
   react(),
 ],
-  site: `https://dembresearch.github.io/`,
-  base: '/demb_docs'
+  site: `https://DEMBresearch.github.io/`
 });
